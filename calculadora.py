c65714351eb11e32d83fbaf392e279c049e5eafe
diggs23 <36986537+diggs23@users.noreply.github.com>
2021-11-05 21:40:36 +0000

    Update calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -1,29 +1,33 @@
-operation = input('''
-Please type in the math operation you would like to complete:
-+ for addition
-- for subtraction
-* for multiplication
-/ for division
-''')
+while (1):
+    operation = input('''
+    Please type in the math operation you would like to complete:
+    + for addition
+    - for subtraction
+    * for multiplication
+    / for division
+    q for exit
+    ''')
+    if operation=='q':
+        break
+    x = int(input('First number: '))
+    y = int(input('Second number: '))
 
-x = int(input('First number: '))
-y = int(input('Second number: '))
+    if operation == '+':
+        print('{} + {} = '.format(x, y))
+        print(x + y)
 
-if operation == '+':
-    print('{} + {} = '.format(x, y))
-    print(x + y)
+    elif operation == '-':
+        print('{} - {} = '.format(x, y))
+        print(x - y)
 
-elif operation == '-':
-    print('{} - {} = '.format(x, y))
-    print(x - y)
+    elif operation == '*':
+        print('{} * {} = '.format(x, y))
+        print(x * y)
 
-elif operation == '*':
-    print('{} * {} = '.format(x, y))
-    print(x * y)
+    elif operation == '/':
+        print('{} / {} = '.format(x, y))
+        print(x / y)
 
-elif operation == '/':
-    print('{} / {} = '.format(x, y))
-    print(x / y)
+    else:
+        print('You have not typed a valid operator, please run the program again.')
 
-else:
-    print('You have not typed a valid operator, please run the program again.')
