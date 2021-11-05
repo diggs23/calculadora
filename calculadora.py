5c73598b62b3b85f97b0ad021c213b98baa0f0e1
diggs23 <36986537+diggs23@users.noreply.github.com>
2021-11-05 18:25:13 +0000

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,29 @@
+operation = input('''
+Please type in the math operation you would like to complete:
++ for addition
+- for subtraction
+* for multiplication
+/ for division
+''')
+
+x = int(input('First number: '))
+y = int(input('Second number: '))
+
+if operation == '+':
+    print('{} + {} = '.format(x, y))
+    print(x + y)
+
+elif operation == '-':
+    print('{} - {} = '.format(x, y))
+    print(x - y)
+
+elif operation == '*':
+    print('{} * {} = '.format(x, y))
+    print(x * y)
+
+elif operation == '/':
+    print('{} / {} = '.format(x, y))
+    print(x / y)
+
+else:
+    print('You have not typed a valid operator, please run the program again.')
